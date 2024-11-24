8719d6e14b53d2fb086240b8002082c4d3a70620
Javier Pacareu <javierpacareu@gmail.com>
2024-11-24 10:46:47 -0800

    feat: add changes

--- supabase/functions/_shared/base-respository.ts
+++ supabase/functions/_shared/base-respository.ts
@@ -4,69 +4,69 @@ import { Database } from "../types.ts";
 type TableName = keyof Database["public"]["Tables"];
 
 export abstract class BaseRepository<T extends TableName> {
-    protected supabase: SupabaseClient<Database>;
-    protected tableName: T;
-    protected table: ReturnType<SupabaseClient<Database>["from"]>;
+  protected supabase: SupabaseClient<Database>;
+  protected tableName: T;
+  protected table: ReturnType<SupabaseClient<Database>["from"]>;
 
-    constructor(supabaseClient: SupabaseClient, tableName: T) {
-        this.supabase = supabaseClient;
-        this.tableName = tableName;
-        this.table = this.supabase.from(tableName);
+  constructor(supabaseClient: SupabaseClient, tableName: T) {
+    this.supabase = supabaseClient;
+    this.tableName = tableName;
+    this.table = this.supabase.from(tableName);
+  }
+
+  // Retrieve all records
+  async getAll() {
+    const { data, error } = await this.supabase.from(
+      this.tableName,
+    ).select(
+      "*",
+    );
+    if (error) throw new Error(error.message);
+    return data || [];
+  }
+
+  // Retrieve a single record by its ID
+  async getById(id: string) {
+    const { data, error } = await this.supabase
+      .from(this.tableName)
+      .select("*")
+      .eq("id", id)
+      .maybeSingle();
+
+    if (error) {
+      if (error.message === "Row not found") return null;
+      throw new Error(error.message);
     }
+    return data;
+  }
 
-    // Retrieve all records
-    async getAll() {
-        const { data, error } = await this.supabase.from(
-            this.tableName,
-        ).select(
-            "*",
-        );
-        if (error) throw new Error(error.message);
-        return data || [];
-    }
+  // Create a new record
+  async create(item: any) {
+    const { data, error } = await this.supabase.from(this.tableName).insert(
+      item,
+    ).maybeSingle();
+    if (error) throw new Error(error.message);
+    return data;
+  }
 
-    // Retrieve a single record by its ID
-    async getById(id: string) {
-        const { data, error } = await this.supabase
-            .from(this.tableName)
-            .select("*")
-            .eq("id", id)
-            .maybeSingle();
+  // Update an existing record
+  async update(id: string, updates: any) {
+    const { data, error } = await this.supabase
+      .from(this.tableName)
+      .update(updates)
+      .eq("id", id)
+      .maybeSingle();
+    if (error) throw new Error(error.message);
+    return data;
+  }
 
-        if (error) {
-            if (error.message === "Row not found") return null;
-            throw new Error(error.message);
-        }
-        return data;
-    }
-
-    // Create a new record
-    async create(item: any) {
-        const { data, error } = await this.supabase.from(this.tableName).insert(
-            item,
-        ).maybeSingle();
-        if (error) throw new Error(error.message);
-        return data;
-    }
-
-    // Update an existing record
-    async update(id: string, updates: any) {
-        const { data, error } = await this.supabase
-            .from(this.tableName)
-            .update(updates)
-            .eq("id", id)
-            .maybeSingle();
-        if (error) throw new Error(error.message);
-        return data;
-    }
-
-    // Delete a record by its ID
-    async delete(id: string) {
-        const { error } = await this.supabase.from(this.tableName).delete().eq(
-            "id",
-            id,
-        );
-        if (error) throw new Error(error.message);
-        return true;
-    }
+  // Delete a record by its ID
+  async delete(id: string) {
+    const { error } = await this.supabase.from(this.tableName).delete().eq(
+      "id",
+      id,
+    );
+    if (error) throw new Error(error.message);
+    return true;
+  }
 }
---
+++ supabase/functions/users/service.ts
@@ -0,0 +1,40 @@
+import { SupabaseClient } from "jsr:@supabase/supabase-js@2";
+import { UserRepository } from "../_repositories/user.ts";
+import { InsertPayload } from "../types.custom.ts";
+
+export default class UserService {
+  private userRepository: UserRepository;
+
+  constructor(supabaseClient: SupabaseClient) {
+    this.userRepository = new UserRepository(supabaseClient);
+  }
+
+  createUser = async (
+    body: InsertPayload<{
+      id: string;
+      email: string;
+    }>,
+  ) => {
+    await this.userRepository.create(body.record);
+
+    const { id, email } = body.record;
+
+    return this.getResponseByEvent("USER_CRETED", {
+      id,
+      email,
+    });
+  };
+
+  getResponseByEvent = (event: string, data?: any) => {
+    switch (event) {
+      case "USER_CRETED":
+        return ({
+          event: "USER_CRETED",
+          payload: {
+            message: "User created",
+            ...data,
+          },
+        });
+    }
+  };
+}
